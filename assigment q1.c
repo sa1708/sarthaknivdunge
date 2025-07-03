ad4708d7a9137f2c3a32bc3712c4c2b28379e4ad
sa1708 <sarthaknivdunge26@gmail.com>
2025-07-03 13:00:39 +0530

    Add files via upload

---
+++ assigment q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	
+ int num1, num2, sum;
+ printf("enter two integers:");
+ 
+ scanf("%d %d", &num1 , num2);
+ sum = num1 + num2; 
+printf(" sum: %d\n", sum);
+  return 0;
+  
+}
