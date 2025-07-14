d6445d1fa1f2184d352280082a386bc983c2eb47
sa1708 <sarthaknivdunge26@gmail.com>
2025-07-14 12:41:08 +0530

    Add files via upload

---
+++ assigment 2/Q3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int side1, side2, side3;
+
+    
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &side1, &side2, &side3);
+
+    
+    if ((side1 + side2 > side3) &&
+        (side2 + side3 > side1) &&
+        (side1 + side3 > side2)) {
+        
+        
+        if (side1 == side2 && side2 == side3) {
+            printf("The triangle is Equilateral.\n");
+        } else if (side1 == side2 || side2 == side3 || side1 == side3) {
+            printf("The triangle is Isosceles.\n");
+        } else {
+            printf("The triangle is Scalene.\n");
+        }
+
+    } else {
+        printf("The given sides do not form a valid triangle.\n");
+    }
+
+    return 0;
+}
