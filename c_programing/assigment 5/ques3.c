37903fcf7c41aca3e5cf4f2cd3937c5225d76fc2
sa1708 <sarthaknivdunge26@gmail.com>
2025-07-14 12:47:37 +0530

    Add files via upload

---
+++ c_programing/assigment 5/ques3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	for (int i=1; i<=5; i++)
+	{
+     	for(int j = 1;j<=i; j++)
+    	{
+    		if(i==5||j==5||i+j|i==j&&i>5)
+		printf("* ");
+	    }
+			printf("\n");
+		}
+		}
+
+		
+    
+   
+
+		
+		
+	
