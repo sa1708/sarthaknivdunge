be6671450bad0e24e34b5ac0be3011d7b4446e62
sa1708 <sarthaknivdunge26@gmail.com>
2025-07-19 16:10:33 +0530

    Add files via upload

---
+++ c_programing/assigment 7/que3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void vowel();
+void main ()
+{
+	char ch='a' ;
+   printf("enter any word ");
+   scanf("%c",&ch);
+   vowel(&ch);
+}
+void vowel( char *ch)
+{	
+    if (*ch=='a'||*ch=='e'||*ch=='i'|| *ch=='o'||*ch=='u') 
+    {
+    	printf("this  is vowel");
+    	
+	}
+	else
+	{
+		printf("this is consonant");
+	}
+	
+}
